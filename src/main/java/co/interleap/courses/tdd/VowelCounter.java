1f8dc0b084453a5b54720f57f1034786b7798de6
neelakshipunia <neelakshi00p@gmail.com>
2021-07-03 23:09:42 +0530

    Count_Vowel

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,4 +1,7 @@
 package co.interleap.courses.tdd;
 
 public class VowelCounter {
+    public int count(String abc){
+        return abc.length();
+    }
 }
--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -1,9 +1,25 @@
 package co.interleap.courses.tdd;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class VowelCounterTest {
 
     @Test
-    public void dummy(){}
+    public void testWhenThereIsNoNullString(){
+        VowelCounter vowelCounter = new VowelCounter();
+        assertEquals(0,vowelCounter.count(""));
+    }
+
+    @Test
+    public void WhenStringContainOnlyOneVowelNoConsonant(){
+        VowelCounter vowelCounter = new VowelCounter();
+        assertEquals(1,vowelCounter.count("u"));
+    }
+
+    @Test
+    public void WhenStringContainManyVowelNoConsonant(){
+        VowelCounter vowelCounter = new VowelCounter();
+        assertEquals(5,vowelCounter.count("aeiou"));
+    }
 }
